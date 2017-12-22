af1a9474c921979753de28d7668f2a4ce7db55eb
Javier Ubillos <javier@ubillos.org>
2017-12-22 08:15:25 +0100

    Added command-args boilerplate
    Added code from https://stackoverflow.com/questions/3609852/which-is-the-best-way-to-allow-configuration-options-be-overridden-at-the-comman
    Touched file config.ini and changed the var 'option' to minercmd to get some first outputs.

---
+++ minerrestarter.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+import argparse
+import ConfigParser
+import json
+import sys
+
+def main(argv=None):
+    # Do argv default this way, as doing it in the functional
+    # declaration sets it at compile time.
+    if argv is None:
+        argv = sys.argv
+
+    # Parse any conf_file specification
+    # We make this parser with add_help=False so that
+    # it doesn't parse -h and print help.
+    conf_parser = argparse.ArgumentParser(
+        description=__doc__, # printed with -h/--help
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        # Turn off help, so we print all options in response to -h
+        add_help=False
+        )
+    conf_parser.add_argument("-c", "--conf_file",
+                        help="Specify config file", default="config.ini", metavar="FILE")
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    defaults = { "option":"default" }
+
+    if args.conf_file:
+        config = ConfigParser.SafeConfigParser()
+        config.read([args.conf_file])
+        defaults.update(dict(config.items("Defaults")))
+
+    # Parse rest of arguments
+    # Don't suppress add_help here so it will handle -h
+    parser = argparse.ArgumentParser(
+        # Inherit options from config_parser
+        parents=[conf_parser]
+        )
+    parser.set_defaults(**defaults)
+    parser.add_argument("--minercmd")
+    args = parser.parse_args(remaining_argv)
+    print "Minercmd is \"{}\"".format(args.minercmd)
+    return(0)
+
+if __name__ == "__main__":
+    sys.exit(main())
